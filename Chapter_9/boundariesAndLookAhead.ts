96fb640779458e19ea36c3a232d534b3bd312eb6
DanielPini <daniel@danielpini.com>
2026-05-21 16:49:02 +1000

    Update boundaries and look ahead file

--- Chapter_9/boundariesAndLookAhead.ts
+++ Chapter_9/boundariesAndLookAhead.ts
@@ -94,3 +94,57 @@ function stripCommentsNonGreedily(code: any) {
 console.log(stripCommentsNonGreedily("1 /* a */+/* b */ 1")); // 1 + 1
 
 // Dynamically creating RegExp objects
+// In some cases you may not know the exact pattern you need to match against when you start writing your code.
+// Say you want to test for the user's name in a piece of text, you can build up a string and use the RegExp constructor on that
+let firstName: string = "harry";
+let regexp = new RegExp("(^|\\s)" + firstName + "($|\\s)", "gi");
+console.log(regexp.test("Harry is a dodgy character.")); // true
+// Note: when writing the \s part of the string, we need to use two backslashes as we are writing this in a normal string, not in a backslash-encoded regular expression
+
+// What do we do if the name is "dea+hl[]rd" because our useer is an edgelord?
+// To work around this, we can add backslashes before any character that has a special meaning
+let edgeLord: string = "dea+hl[]rd";
+let escaped: string = edgeLord.replace(/[\\[.+*?(){|^$]/g, "\\$&");
+let edgelordRegexp = new RegExp("(^|\\s)" + escaped + "($|\\s)", "gi");
+let text = "This dea+hl[]rd guy is super annoying.";
+console.log(edgelordRegexp.test(text)); // true
+
+// search()
+// While the indexOf() method on strings can't be called with regular expressions, there is another method, search() that does expect a regular expression.
+// Like indexOf() it returns the first index that is found or -1
+console.log(" word".search(/\S/)); // 2
+console.log(".   ".search(/\S/)); // -1
+// search(), unfortunately, does not allow the search to start at a specific index
+
+// lastIndex property
+// The exec() method does not provide a convenient way to start searching from a given position in the string.
+// But it does provide an inconvenient way
+// Regular Expression objects have properties.
+// One such property is source, which contains the string that that expression is taken from
+// Another property is lastIndex, which controls in some limited circumstances, where the next match will start.
+// Those circumstances are that the regular expression must have the global (g), or sticky (y) option enabled, and the match must happen through the exec method.
+// Again, a less confusing solution would have been to just allow an extra argument to be passed to exec, but confusion is an essential feature of JavaScript's regular expression interface.
+let pattern = /y/g;
+pattern.lastIndex = 3;
+let lastIndexMatch = pattern.exec("xyzzy");
+if (lastIndexMatch) console.log(lastIndexMatch.index); // 4
+console.log(pattern.lastIndex); // 5
+// If the match was successful, the call to exec automatically updates the lastIndex property to point after the match. If no match was found, lastIndex is set back to 0, which is also the value it has in a newly constructed regular expression object.
+
+// The difference between the global and sticky options is that when sticky is enabled, the match will succeed only if it starts directly at lastIndex, whereas with global, it will search ahead for a position where a match can start
+let global = /abc/g;
+console.log(global.exec("xyz abc")); // ["abc"]
+let sticky = /aby/y;
+console.log(sticky.exec("xyz abc")); // null
+
+// When using a shared regular expression value for multiple exec calls, these autmoatic updates to lastIndex property can cause problems.
+// Your regular expression might be accidentally starting at an index left over from a previous call.
+let digit = /\d/g;
+console.log(digit.lastIndex); // 0
+console.log(digit.exec("here it is: 1")); // ["1"]
+console.log(digit.lastIndex); // 13
+console.log(digit.exec("and now: 1")); // null
+
+// Another interesting effect of the global option is that it changes the way the match method works on strings.
+// When called with a global expression, instead of returning an array similar to theat returned by exec, match will find all matches of the pattern in the string and return an array containing the matched strings
+console.log("Banana".match(/an/g)); // ["an", "an"]
